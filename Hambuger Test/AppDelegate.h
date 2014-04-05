550ea7f3ad9a541fba8d63fbdd6a5136baba7ec7
David Ladowitz <david@ladowitz.com>
2014-04-04 22:44:17 -0700

    Initial Commit

---
+++ Hambuger Test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Hambuger Test
+//
+//  Created by David Ladowitz on 4/4/14.
+//  Copyright (c) 2014 Team1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
